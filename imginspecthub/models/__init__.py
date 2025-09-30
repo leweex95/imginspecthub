0d449040c14c1022f81e929804a970474a488211
leweex95 <csibi.levente14@gmail.com>
2025-09-30 09:39:46 +0200

    removed non-working models from registry

--- imginspecthub/models/__init__.py
+++ imginspecthub/models/__init__.py
@@ -1,22 +1,18 @@
 """
-Model implementations for various image understanding models.
+Model implementations for visual question answering.
 """
 
 from .base import BaseModel
 from .registry import ModelRegistry, get_available_models, get_model
 
-# Import models to trigger registration
-from .clip_model import CLIPModel
+# Import only working models to trigger registration
 from .blip2_model import BLIP2Model
 from .llava_model import LLaVAModel
-from .minigpt4_model import MiniGPT4Model
 
 __all__ = [
     "BaseModel", 
-    "CLIPModel", 
     "BLIP2Model", 
-    "LLaVAModel", 
-    "MiniGPT4Model",
+    "LLaVAModel",
     "ModelRegistry", 
     "get_available_models", 
     "get_model"
